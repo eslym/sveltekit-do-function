07ef76bab1daaf21bbe6606e385b483caa18bf22
0nepeop1e <engshun.lim@gmail.com>
2023-04-16 14:52:46 +0800

    some adjustment

--- packages/sveltekit/endpoint/svelte.config.js
+++ packages/sveltekit/endpoint/svelte.config.js
@@ -12,8 +12,11 @@ const config = {
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
-            base_url: process.env.BASE_URL,
-        })
+            base_url: process.env.BASE_URL
+        }),
+        paths: {
+            base: '/sveltekit/endpoint'
+        }
     }
 };
 
