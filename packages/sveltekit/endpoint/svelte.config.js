318161463ffc760c7fa4dac7a106203e177fe136
0nepeop1e <engshun.lim@gmail.com>
2023-04-16 15:38:09 +0800

    adjust again

--- packages/sveltekit/endpoint/svelte.config.js
+++ packages/sveltekit/endpoint/svelte.config.js
@@ -13,10 +13,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             base_url: process.env.BASE_URL
-        }),
-        paths: {
-            base: '/sveltekit/endpoint'
-        }
+        })
     }
 };
 
