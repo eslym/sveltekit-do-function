3f2ca1688ce58e4d13be2c475ff1283ca332eba7
0nepeop1e <engshun.lim@gmail.com>
2023-04-17 00:44:59 +0800

    update dependencies

--- packages/sveltekit/endpoint/svelte.config.js
+++ packages/sveltekit/endpoint/svelte.config.js
@@ -12,8 +12,7 @@ const config = {
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
-            base_url: process.env.BASE_URL,
-            ip_header: 'do-connecting-ip'
+            ipHeader: 'do-connecting-ip'
         })
     }
 };
